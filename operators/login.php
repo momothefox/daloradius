27e014df62ee6ba9aebf98c31ff09e99c486abe6
WitRADIUS <m.ismael@gmail.com>
2023-03-09 04:08:07 +0200

    Update login.php

--- operators/login.php
+++ operators/login.php
@@ -45,7 +45,7 @@ $dir = (strtolower($langCode) === 'ar') ? "rtl" : "ltr";
 <html lang="<?= $langCode ?>" dir="<?= $dir ?>">
 
 <head>
-    <title>daloRADIUS :: Login</title>
+    <title>WitRADIUS :: Login</title>
     <meta charset="utf-8">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
